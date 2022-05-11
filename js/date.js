96b1cfd02c3639f22d11e84cce5938065482d864
jjiv <43815804+lynn2134@users.noreply.github.com>
2022-05-11 23:48:06 +0900

    0511
    찌브찌도 페이지
    메뉴바와 페이지 전환

---
+++ js/date.js
@@ -0,0 +1,20 @@
+const date = document.querySelector("div#date");
+const days = document.querySelector("div#days");
+
+function getDate(){
+  const weeks = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"];
+  const theDate = new Date();
+  const year = theDate.getFullYear();
+  const month = theDate.getMonth()+1;
+  const day = theDate.getDate();
+  const week = weeks[theDate.getDay()];
+  date.innerText = `안녕! 오늘은 ${year}년 ${month}월 ${day}일 ${week}이야`;
+
+ const stDate = new Date(2017,03,20);
+ const endDate = new Date(year, month, day);
+ const btMonth = endDate.getTime() - stDate.getTime();
+ const btDay = btMonth / (1000*60*60*24);
+ days.innerText = `우리가 만난지 ${btDay+1}일 되는 날이지!`;
+}
+
+getDate();
